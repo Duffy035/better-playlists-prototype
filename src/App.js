58dbbfa49fe832a1fadd1dafd8de69a9aa24ec8e
C. Andersson <hejjacarl@hotmail.com>
2018-01-11 14:24:24 +0100

    dynamic listing of fake server playlist data

--- src/App.js
+++ src/App.js
@@ -85,13 +85,15 @@ class Filter extends Component{
 
 class Playlist extends Component{
   render() {
+    let playlist = this.props.playlist
     return (
       <div style={{...deafultStyle, display: 'inline-block', width: '25%'}}>
         <img/>
-        <h3>Playlist Name</h3>
-        <ul><li>Song 1</li></ul>
-        <ul><li>Song 2</li></ul>
-        <ul><li>Song 3</li></ul>
+        <h3>{playlist.name}</h3>
+        <ul>
+          {playlist.songs.map(song =>
+          <li>{song.name}</li>)}
+        </ul>
       </div>
     )
   }
@@ -108,6 +110,13 @@ class App extends Component {
     }, 2000);
   }
   render() {
+
+/*     let playlistElement = []
+    if (this.state.serverData.user) {
+      this.state.serverData.user.playlists.forEach(playlist =>
+      playlistElement.push(<Playlist playlist={playlist} />)
+    )
+  } */
     return (
       <div className="App">
       
@@ -119,10 +128,10 @@ class App extends Component {
             <PlaylistCounter playlists= {this.state.serverData.user.playlists}/>
             <HoursCounter playlists= {this.state.serverData.user.playlists}/>
             <Filter/>
-            <Playlist/>
-            <Playlist/>
-            <Playlist/>
-            <Playlist/>
+           {/* {playlistElement} */}
+            {this.state.serverData.user.playlists.map(playlist =>
+            <Playlist playlist={playlist}/>
+            )}
         </div> : <h1 style={deafultStyle}>Loading...</h1>
         }
       </div>
